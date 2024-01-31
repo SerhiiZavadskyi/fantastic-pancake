e8d87b6128451062ca6f029b622e0b1bdb4a496c
Serhii Zavadskyi <serhii.zavadskyi@gmail.com>
2024-01-31 11:10:06 +0200

    FizzBuzz

---
+++ 412-FizzBuzz.ts
@@ -0,0 +1,37 @@
+function fizzBuzz2(n: number): string[] {
+	const res: string[] = new Array(n);
+
+	for (let i = 1; i <= n; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			res[i - 1] = "FizzBuzz";
+		} else if (i % 3 === 0) {
+			res[i - 1] = "Fizz";
+		} else if (i % 5 === 0) {
+			res[i - 1] = "Buzz";
+		} else {
+			res[i - 1] = i.toString();
+		}
+	}
+
+	return res;
+}
+
+function fizzBuzz(n: number): string[] {
+	const res: string[] = [];
+
+	for (let i = 1; i <= n; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			res.push("FizzBuzz");
+		} else if (i % 3 === 0) {
+			res.push("Fizz");
+		} else if (i % 5 === 0) {
+			res.push("Buzz");
+		} else {
+			res.push(i.toString());
+		}
+	}
+
+	return res;
+}
+
+console.log(fizzBuzz(5));
