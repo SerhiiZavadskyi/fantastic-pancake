ea2a0ed7fbb06292d5a9d078cc6e6fe587f7a0a1
Serhii Zavadskyi <serhii.zavadskyi@gmail.com>
2024-06-02 11:22:59 +0300

    Day 185 | June 2, 2024

--- 0344_ReverseString.ts
+++ 0344_ReverseString.ts
@@ -2,18 +2,26 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-	let l = 0;
-	let r = s.length - 1;
+	let l = 0
+	let r = s.length - 1
 
 	while (l < r) {
-		const tail = s[r];
-		s[r] = s[l];
-		s[l] = tail;
-		l++;
-		r--;
+		const tail = s[r]
+		s[r] = s[l]
+		s[l] = tail
+		l++
+		r--
 	}
 
-	console.log(s);
+	console.log(s)
 }
 
-reverseString(["h", "e", "l", "l", "o"]);
+function reverseString2(s: string[]): void {
+	for (let l = 0; l < s.length / 2; l++) {
+		const r = s.length - 1 - l
+		const temp = s[l]
+		s[l] = s[r]
+		s[r] = temp
+	}
+}
+reverseString(["h", "e", "l", "l", "o"])
