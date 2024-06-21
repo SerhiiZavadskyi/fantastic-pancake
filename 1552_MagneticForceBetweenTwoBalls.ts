8f1e7d952d5e570686e6e21d08245e11a182d8ff
Serhii Zavadskyi <serhii.zavadskyi@gmail.com>
2024-06-21 09:25:25 +0300

    Day 203 | June 20, 2024

---
+++ 1552_MagneticForceBetweenTwoBalls.ts
@@ -0,0 +1,34 @@
+function maxDistance(position: number[], m: number): number {
+	position.sort((a, b) => a - b)
+	let l = 0,
+		r = position[position.length - 1] - position[0],
+		answer = -1
+	while (l <= r) {
+		const mid = ((l + r) / 2) | 0
+
+		if (canPlaceBalls(position, m, mid)) {
+			answer = mid
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+
+	return answer
+}
+
+function canPlaceBalls(positions: number[], m: number, mid: number): boolean {
+	let balls = 1,
+		position = positions[0]
+
+	for (let i = 1; i < positions.length; i++) {
+		if (positions[i] - position >= mid) {
+			balls++
+			position = positions[i]
+			if (balls === m) {
+				return true
+			}
+		}
+	}
+	return false
+}
