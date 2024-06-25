0c6d55553f9367dbd6baf3925f0bcb4ed042541e
Serhii Zavadskyi <serhii.zavadskyi@gmail.com>
2024-06-25 15:12:51 +0300

    Day 208 | June 25, 2024

---
+++ 1038_BinarySearchTreeToGreaterSumTree.ts
@@ -0,0 +1,34 @@
+import { inspect } from "util"
+import { drawTree } from "./helpers/drawTree"
+import { TreeNode, BinarySearchTree } from "./helpers/Tree"
+
+function bstToGst(root: TreeNode | null): TreeNode | null {
+	let currSum = 0
+
+	function dfs(node: TreeNode | null) {
+		if (!node) {
+			return
+		}
+
+		if (node.right) {
+			dfs(node.right)
+		}
+		const temp = node.val
+		node.val += currSum
+		currSum += temp
+
+		if (node.left) {
+			dfs(node.left)
+		}
+	}
+	dfs(root)
+	return root
+}
+
+const tree = new TreeNode(
+	4,
+	new TreeNode(1, new TreeNode(0), new TreeNode(2, null, new TreeNode(3))),
+	new TreeNode(6, new TreeNode(5), new TreeNode(7, null, new TreeNode(8)))
+)
+
+console.log(bstToGst(tree))
